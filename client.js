2513d5e4eec1de558c694ac6c09161a97ad1cf02
Glonel Dimapilis <gdimapilis@aol.com>
2023-02-15 01:49:54 -0600

    got the submitForm function to store user inputs

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
 console.log(`Wassup World?!`);
+
+// function to pull information from user input on submit
+function submitForm(event) {
+    event.preventDefault();
+    let firstName = document.querySelector('#firstNameInput').value;
+    let lastName = document.querySelector('#lastNameInput').value;
+    let idNumber = document.querySelector('#idInput').value;
+    let jobTitle = document.querySelector('#jobTitleInput').value;
+    let annualSal = Number(document.querySelector('#salaryInput').value);
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(idNumber);
+    console.log(jobTitle);
+    console.log(annualSal);
+
+}
