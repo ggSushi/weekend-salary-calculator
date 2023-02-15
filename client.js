bf13baacbcb5f8659b2d2ead393e962fe9f26ad2
Glonel Dimapilis <gdimapilis@aol.com>
2023-02-15 03:11:44 -0600

    added some notes

--- client.js
+++ client.js
@@ -8,6 +8,7 @@ let monthlyDiv = document.querySelector('#total-monthly');
 // function to pull information from user input on submit
 function submitForm(event) {
     event.preventDefault();
+    
     // storing inputs into variables
     let employeeDiv = document.querySelector('#employee-list');
     let firstName = document.querySelector('#firstNameInput').value;
@@ -15,11 +16,14 @@ function submitForm(event) {
     let idNumber = document.querySelector('#idInput').value;
     let jobTitle = document.querySelector('#jobTitleInput').value;
     let annualSal = Number(document.querySelector('#salaryInput').value);
+
     // store monthly calculations
     let monthlyCost = annualSal / 12;
     totalMonthlyCost += monthlyCost;
+
+    // This .innerHTML will append the totalMonthlyCost to the <p> tag inside the #total-monthly div
     monthlySpan.innerHTML = `${totalMonthlyCost.toFixed(2)}`;
-    //! parseFloat(var.toFixed(2)); will round to the second decimal place, as indicated by the 2
+    //! parseFloat(var.toFixed(2)); will round to the second decimal place, as indicated by the (2)
     employeeDiv.innerHTML += `
         <tr>
             <td>${lastName}</td>
@@ -31,6 +35,7 @@ function submitForm(event) {
             <td><button id="delete-button" onclick="deleteRow(event)" >Delete</button></td>
         </tr>
     `  
+    // This will change the bg color of the total monthly cost div to red if it exceeds 20000
     if (totalMonthlyCost > 20000) {
         monthlyDiv.style.backgroundColor = "red";
     }
