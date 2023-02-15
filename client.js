3141207b37ae869b87463ded1f684c315ec4556b
Glonel Dimapilis <gdimapilis@aol.com>
2023-02-15 02:16:20 -0600

    Got the submitForm function to append information to the DOM and calculate monthly cost

--- client.js
+++ client.js
@@ -3,16 +3,25 @@ console.log(`Wassup World?!`);
 // function to pull information from user input on submit
 function submitForm(event) {
     event.preventDefault();
+    // storing inputs into variables
+    let employeeDiv = document.querySelector('#employee-list');
     let firstName = document.querySelector('#firstNameInput').value;
     let lastName = document.querySelector('#lastNameInput').value;
     let idNumber = document.querySelector('#idInput').value;
     let jobTitle = document.querySelector('#jobTitleInput').value;
     let annualSal = Number(document.querySelector('#salaryInput').value);
+    // store monthly calculations
+    let monthlyCost = annualSal / 12;
 
-    console.log(firstName);
-    console.log(lastName);
-    console.log(idNumber);
-    console.log(jobTitle);
-    console.log(annualSal);
-
+    employeeDiv.innerHTML += `
+        <tr>
+            <td>${lastName}</td>
+            <td>${firstName}</td>
+            <td>${idNumber}</td>
+            <td>${jobTitle}</td>
+            <td>${annualSal}</td>
+            <td>${monthlyCost}</td>
+            <td><button id="delete-button" onclick="deleteRow(event)" >Delete</button></td>
+        </tr>
+    `
 }
