d8bdf43b64585fc12b3df7b2c07c8e24ad159446
Glonel Dimapilis <gdimapilis@aol.com>
2023-02-15 05:18:57 -0600

    got the selection for the delete function working. Stretch goals complete

--- client.js
+++ client.js
@@ -4,8 +4,7 @@ let totalMonthlyCost = 1666.67;
 let monthlySpan = document.querySelector('.starting-monthly');
 let monthlyDiv = document.querySelector('#total-monthly');
 
-
-// function to pull information from user input on submit
+// function to pull information from user input on submit on the form
 function submitForm(event) {
     event.preventDefault();
 
@@ -17,9 +16,9 @@ function submitForm(event) {
     let jobTitle = document.querySelector('#jobTitleInput').value;
     let annualSal = Number(document.querySelector('#salaryInput').value);
 
-    // store monthly calculations
-    let monthlyCost = annualSal / 12;
-    totalMonthlyCost += monthlyCost;
+     // store monthly calculations
+     let monthlyCost = annualSal / 12;
+     totalMonthlyCost += monthlyCost;
 
     // This .innerHTML will append the totalMonthlyCost to the <p> tag inside the #total-monthly div
     monthlySpan.innerHTML = `${totalMonthlyCost.toFixed(2)}`;
@@ -31,7 +30,7 @@ function submitForm(event) {
             <td>${idNumber}</td>
             <td>${jobTitle}</td>
             <td>${annualSal}</td>
-            <td class="employee-cost">${parseFloat(monthlyCost.toFixed(2))}</td>
+            <td class="cost">${parseFloat(monthlyCost.toFixed(2))}</td>
             <td><button id="delete-button" onclick="deleteRow(event)" >Delete</button></td>
         </tr>
     `  
@@ -43,6 +42,13 @@ function submitForm(event) {
 
 // This function will delete the row and subtract from the total monthly cost
 function deleteRow(event) {
+    let cost = event.target.parentElement.previousElementSibling;
+    console.log(cost);
+    let subCost = Number(cost.innerHTML);
+    console.log(subCost);
+    totalMonthlyCost -= subCost;
+
+    monthlySpan.innerHTML = `${totalMonthlyCost.toFixed(2)}`;
     event.target.parentElement.parentElement.remove();
     if (totalMonthlyCost < 20000) {
         //! setting color to "transparent" or "initial" will clear the color to its default state"
@@ -50,7 +56,8 @@ function deleteRow(event) {
     }
 }
 
-// function to clear the input fields after submit
+//! You can add two different functions two a single onClick or onSubmit event
+// function to clear the input fields after submit in the form
 function clearInput(event) {
     //! .reset() is a funciton that will reset all input fields inside of a form
     event.target.reset();
